4e37483769744551b2cd8f76404fc9eb8730be13
Vlad 'mend0za' Shakhov <lumpen.intellectual@gmail.com>
2011-11-04 22:04:48 +0300

    pkgsrc: finalize host-pkgsrc, start with target-pkgsrc packages building

--- package/pkgsrc/pkgsrc.mk
+++ package/pkgsrc/pkgsrc.mk
@@ -9,10 +9,8 @@ ifeq ($(BR2_PKGSRC),y)
 PKGSRC_VERSION = $(BR2_PKGSRC_VERSION)
 PKGSRC_SOURCE_DIR := $(BUILD_DIR)/host-pkgsrc-$(PKGSRC_VERSION)
 PKGSRC_HOST_DIR := $(HOST_DIR)/pkgsrc-$(PKGSRC_VERSION)
-PKGSRC_DEST_DIR := $(TARGET_DIR)/usr/pkg
-PKGSRC_WORK_DIR := $(BUILD_DIR)/pkgsrc-workdir
 
 include package/pkgsrc/host-pkgsrc.mk.in
-include package/pkgsrc/packages.mk.in
+include package/pkgsrc/target-pkgsrc.mk.in
 
 endif # BR2_PKGSRC
