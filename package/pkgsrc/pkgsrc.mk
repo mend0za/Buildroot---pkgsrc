7532bfc6e024cf98ec35a90e388c61242a1b7a8e
Vlad 'mend0za' Shakhov <lumpen.intellectual@gmail.com>
2011-11-13 21:38:07 +0300

    pkgsrc: bootstrap (host) and platform tools (host) build verified

--- package/pkgsrc/pkgsrc.mk
+++ package/pkgsrc/pkgsrc.mk
@@ -10,6 +10,12 @@ PKGSRC_VERSION = $(BR2_PKGSRC_VERSION)
 PKGSRC_SOURCE_DIR := $(BUILD_DIR)/host-pkgsrc-$(PKGSRC_VERSION)
 PKGSRC_HOST_DIR := $(HOST_DIR)/pkgsrc-$(PKGSRC_VERSION)
 
+ifeq ($(BR2_CCACHE),y)
+	PKGSRC_COMPILER = ccache gcc
+else
+	PKGSRC_COMPILER = gcc
+endif
+
 include package/pkgsrc/host-pkgsrc.mk.in
 include package/pkgsrc/target-pkgsrc.mk.in
 
